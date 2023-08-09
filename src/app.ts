fa8343f2afe4bfbb4902d1d9ae390cf0df9a1476
Camilo Andrés Frontado Escobar <redesjavacamilo@gmail.com>
2023-08-09 11:26:11 +0100

    come back lastest version

--- src/app.ts
+++ src/app.ts
@@ -26,10 +26,10 @@ class App {
   }
 
   private routes(): void {
-    this.app.use('/', (req, res) => { res.status(200).send()}); // aws health
     this.app.use('/api/v1/contacts', contactsRouter);
     this.app.use('/api/v1/login', loginRouter);
     this.app.use('/api/v1/stripe', stripeRouter);
+    this.app.use('/', (req, res) => { res.status(200).send()}); // aws health
   }
 
   public start(): void {
